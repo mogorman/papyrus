990f3f330d8f67de71d42d25f1241ed7f19ab92d
Matthew O'Gorman <mog@rldn.net>
2015-01-21 10:21:07 -0600

    started working on my pretty image of the netlist.

--- firmware/src/papyrus.v
+++ firmware/src/papyrus.v
@@ -1,20 +1,21 @@
 /*playing with verilog */
 
 module top (
-    input clk50,
-    output [1:0] led
+	    input 	 clk50,
+	    output [1:0] led,
+	    output 	 blink
 );
 
-    reg data1 = 1'b1;
-    reg [32:0] counter;
-    reg state;
+   reg 			 data1 = 1'b1;
+   reg [32:0] 		 counter;
+   reg 			 state;
     
-    assign led[0] = state;
-    assign led[1] = data1;
-    
-    always @ (posedge clk50) begin
-        counter <= counter + 1;
-        state <= counter[26];
-    end
+   assign led[0] = state;
+   assign led[1] = data1;
+   assign blink = ! state;
+   always @ (posedge clk50) begin
+      counter <= counter + 1;
+      state <= counter[26];
+   end
 
 endmodule
