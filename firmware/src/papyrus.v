37daa7f63929f3fc6837da3766c8e9c847fff502
Matthew O'Gorman <mog@rldn.net>
2015-01-21 09:34:01 -0600

    added something to "erase" spi aka just put the flasher program in the spi.  changed helloworld from vhdl to verilog.  This project will be a verilog project eventually.

---
+++ firmware/src/papyrus.v
@@ -0,0 +1,19 @@
+/*playing with verilog
+module top (
+    input clk50,
+    output [1:0] led
+);
+
+    reg data1 = 1'b1;
+    reg [32:0] counter;
+    reg state;
+    
+    assign led[0] = state;
+    assign led[1] = data1;
+    
+    always @ (posedge clk50) begin
+        counter <= counter + 1;
+        state <= counter[26];
+    end
+
+endmodule
