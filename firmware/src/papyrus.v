b66da999a7d6aca6b6547cd56ea629f3d007f6a4
Matthew O'Gorman <mog@rldn.net>
2015-01-21 10:57:51 -0600

    playing with verilog code and updating continuous integration script.

--- firmware/src/papyrus.v
+++ firmware/src/papyrus.v
@@ -7,15 +7,20 @@ module top (
 );
 
    reg 			 data1 = 1'b1;
-   reg [32:0] 		 counter;
+   reg [27:0] 		 counter;
+   reg [128:0] 		 other_counter;
    reg 			 state;
+   reg 			 other_state;
+			 
     
    assign led[0] = state;
    assign led[1] = data1;
-   assign blink = ! state;
+   assign blink = other_state;
    always @ (posedge clk50) begin
       counter <= counter + 1;
+      other_counter <= other_counter + 1;
       state <= counter[26];
+      other_state <= other_counter[26];
    end
 
 endmodule
