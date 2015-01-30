ebf21eb6aaa151f472ea23c869b1e62477ed97cc
Matthew O'Gorman <mog@rldn.net>
2015-01-30 13:32:21 -0600

    removing comments

--- firmware/src/papyrus.v
+++ firmware/src/papyrus.v
@@ -16,30 +16,12 @@ module top (
 
    assign leds[5:0] = slow_counter[5:0];
    
-/* -----\/----- EXCLUDED -----\/-----
-   assign leds[0] = slow_counter[0];
-   assign leds[1] = slow_counter[1];
-   assign leds[2] = slow_counter[2];
-   assign leds[3] = slow_counter[3];
-   assign leds[4] = slow_counter[4];
-   assign leds[5] = slow_counter[5];
- -----/\----- EXCLUDED -----/\----- */
-   
    always @ (posedge clk50) begin
       counter <= counter + 1;
       slow_counter <= counter[`mogtest] + slow_counter;
       if (slow_counter == 31) begin
 	 slow_counter <= 0;
 	 state  <= ~state;
-	 
-/* -----\/----- EXCLUDED -----\/-----
-	 if(state != 1'b0) begin
-	    state <= 1'b0;
-	 end
-	 else begin
-	    state <= 1'b1;
-	 end
- -----/\----- EXCLUDED -----/\----- */
       end
       
       if( counter[`mogtest] == 1)
