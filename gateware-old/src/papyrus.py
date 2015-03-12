b64ec4528c209f05874ee6df0726c4e9bb66936f
Matthew O'Gorman <mog@rldn.net>
2015-03-12 04:15:32 -0500

    getting rid of the older work

--- gateware-old/src/papyrus.py
+++
@@ -1,9 +0,0 @@
-from migen.fhdl.std import *
-from mibuild.platforms import mini_spartan_6
-plat = mini_spartan_6.Platform()
-led = plat.request("user_led")
-m = Module()
-counter = Signal(26)
-m.comb += led.eq(counter[25])
-m.sync += counter.eq(counter + 1)
-plat.build_cmdline(m)
